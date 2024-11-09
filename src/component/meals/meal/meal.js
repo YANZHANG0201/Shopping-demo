d82b138c3bc33d020b604d258f3e50b82752e73f
Yan Zhang <149005773+YANZHANG0201@users.noreply.github.com>
2024-11-09 20:31:39 +0000

    practice-demo_start

---
+++ src/component/meals/meal/meal.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./meal.module.css";
+
+const Meal = () => {
+  return <div className={classes.Meal}>Food</div>;
+};
+
+export default Meal;
---
+++ src/component/meals/meals.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Meal from "./meal/meal";
+
+const Meals = () => {
+  return (
+    <div>
+      <Meal />
+    </div>
+  );
+};
+
+export default Meals;
